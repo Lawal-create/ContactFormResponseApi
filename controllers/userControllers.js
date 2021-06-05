0c656b76cfd612a080133e1f79f7996c138f69d5
Lawal Hakeem <lawizyhal@gmail.com>
2021-06-05 14:29:24 +0200

    finaltest

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,15 +1,28 @@
 const express=require("express")
+const { deleteOne } = require("../models/userContactSchema.js")
 express()
 const inquiryList=["I have a question about the service","I have a problem with the API","I have an issue with the product that needs to be resolves urgently","I have a minor problem","My inquiry isn't amongst the ones mentioned above"]
 const schemas=require(`../models/userContactSchema.js`)
 
 
 const getSpecificData=async(req,res)=> {
+
+    let currentDay=new Date().getDay().toString()
+    let currentMonth=new Date().getMonth().toString()
+    let currentYear=new Date().getFullYear().toString()
+
+    const search_value =req.query
+    
     try{
         //filters the data passed 
-        const search_value =req.query
         const results=await schemas.find(search_value)
+        const count=await schemas.countDocuments(search_value)
+
         const result={
+            Day:currentDay,
+            Month:currentMonth,
+            Year:currentYear,
+            totalNoOfResponses:count,
             Result:results
         }
         res.status(200).json(
